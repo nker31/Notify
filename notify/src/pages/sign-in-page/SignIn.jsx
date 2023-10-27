ab6ba408cd8722436096cc7984079e5f5caf36db
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-10-27 22:48:44 +0700

    Create sign in page

---
+++ notify/src/pages/sign-in-page/SignIn.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './SignIn.css'
+function SignIn() {
+  return (
+    <>
+        <div className="sign-in-page-container">
+            <div className="sign-in-box">
+                <div className="sign-in-box-container">
+                    <h1>Welcome to Notify</h1>
+                    <form>
+                        <input type="email" placeholder="Email"/>
+                        <input type="password" placeholder="Password"/>
+                        <button className="sign-in-button" type="submit">Sign In</button>
+                    </form>
+                    {/* line */}
+                    <div className="sign-in-box-line"></div>
+                    <button className='sign-in-no-account-button'>Not on Notify yet?</button>
+                </div>
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default SignIn
