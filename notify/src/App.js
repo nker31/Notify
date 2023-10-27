2452f4407d69c3d55f2a0670ef136ef4d1ddefec
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-10-27 23:19:48 +0700

    create register page and update
    - created register page
    - added page background image
    - update sign in page

--- notify/src/App.js
+++ notify/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import SignIn from './pages/sign-in-page/SignIn';
+import Register from './pages/register-page/Register';
 
 function App() {
   return (
     <>
       <Navbar/>
       <SignIn/>
+      {/* <Register/> */}
     </>
   );
 }
---
+++ notify/src/pages/register-page/Register.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Register.css'
+
+function Register() {
+  return (
+    <div className='register-bg'>
+        <div className="register-page-container">
+            <div className="register-box">
+                <div className="register-box-container">
+                    <h1>Welcome to Notify</h1>
+                    <form>
+                        <input type="email" placeholder="Email"/>
+                        <input type="text" placeholder="Username"/>
+                        <input type="password" placeholder="Password"/>
+                        <input type="password" placeholder="Confirm Password"/>
+                        <input type="tel" placeholder="Phone number"/>
+
+                        <button className="register-button" type="submit">Register</button>
+                    </form>
+                    {/* line */}
+                    <div className="register-box-line"></div>
+                    <button className='register-no-account-button'>Already a member?</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
