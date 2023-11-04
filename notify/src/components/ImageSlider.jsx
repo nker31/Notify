f21fa2a03e4ded7a45dce853fc6939e5fc2ab956
Phoom <phoombnw@gmail.com>
2023-11-04 09:02:13 +0700

    Donate page v.1
    +add qr

--- notify/src/components/ImageSlider.jsx
+++ notify/src/components/ImageSlider.jsx
@@ -7,26 +7,35 @@ import pic3 from '../images/donate3.jpg';
 const ImageSlider = () => {
   const [selected, setSelected] = useState(1);
 
-  const images = [
-    pic1,
-    pic2,
-    pic3
-  ]
+  const imageDetails = [
+    {
+      image: pic1,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officiis, officia ullam distinctio voluptatem illo vel accusantium voluptatibus placeat, temporibus recusandae dignissimos.",
+    },
+    {
+      image: pic2,
+      description: "Facilis reprehenderit corporis libero nihil, voluptate ab magni dolore facere tempora dignissimos neque? Beatae nobis a excepturi laborum? Ipsam ad omnis asperiores?",
+    },
+    {
+      image: pic3,
+      description: "Iste perspiciatis porro totam, ex unde voluptates quam suscipit aut earum itaque eveniet qui laudantium facere sapiente nemo, distinctio odio voluptatibus cum!",
+    },
+  ];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setSelected((prevSelected) => (prevSelected % images.length) + 1);
+      setSelected((prevSelected) => (prevSelected % imageDetails.length) + 1);
     }, 10000);
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, [imageDetails.length]);
 
   return (
     <div className='slide-container'>
       <div className="slider">
         <div className="slides">
-          {images.map((image, index) => (
+          {imageDetails.map((item, index) => (
             <div key={index} className={`slide ${selected === index + 1 ? 'active' : ''}`}>
-              <img src={image} alt="" style={{ width: '100%' }} />
+              <img src={item.image} alt="" style={{ width: '100%' }} />
             </div>
           ))}
         </div>
@@ -34,13 +43,13 @@ const ImageSlider = () => {
 
       <div className='description'>
         <p className='description-paragraph'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est asperiores pariatur inventore consequatur ex provident et dolores, fugit illo at a architecto quaerat, molestiae molestias
+          {selected > 0 && selected <= imageDetails.length ? imageDetails[selected - 1].description : ""}
         </p>
       </div>
 
       <div className="navigation-container">
         <div className="navigation-manual">
-          {images.map((_, index) => (
+          {imageDetails.map((_, index) => (
             <label key={index} className={`manual-btn ${selected === index + 1 ? 'active' : ''}`} onClick={() => setSelected(index + 1)}></label>
           ))}
         </div>
--- notify/src/pages/donate/Donate.jsx
+++ notify/src/pages/donate/Donate.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import './Donate.css'
-import ImageSlider from '../../components/ImageSlider'
+import React, { useState } from 'react';
+import './Donate.css';
+import ImageSlider from '../../components/ImageSlider';
+import qr from '../../images/qr.jpg';
 
 function Donate() {
+    const [buttonState, setButtonState] = useState({ bgColor: '', text: 'DONATE', showImageSlider: true });
+
+    const handleButtonClick = () => {
+        if (buttonState.text === 'DONATE') {
+            setButtonState({ bgColor: '#202124', text: 'BACK', showImageSlider: false });
+        } else {
+            setButtonState({ bgColor: '', text: 'DONATE', showImageSlider: true });
+        }
+    };
+
     return (
         <div className='donate-page-container'>
-            <p className='total-donation-head-text'>
-                Total raised
-            </p>
+            <p className='total-donation-head-text'>Total raised</p>
             <br />
-            <h2 className='total-donation'>
-                $10,000+
-            </h2>
+            <h2 className='total-donation'>$10,000+</h2>
 
             <div className='donate-body'>
-
                 <div className='donate-body-left'>
-                    <ImageSlider />
+                    {buttonState.showImageSlider ? <ImageSlider /> : <img className='donate-qr' src={qr} alt="QR Code" />}
                 </div>
 
                 <div className='donate-body-right'>
@@ -24,28 +30,31 @@ function Donate() {
                     <br />
                     <div className='donate-info-subhead'>Your donations will go directly to those in need</div>
                     <br />
-                    <div className='donate-info'>We are a non-profit organization dedicated to helping victims of natural disasters.  We are committed to using 100% of donations to provide relief to those affected by disasters</div>
+                    <div className='donate-info'>We are a non-profit organization dedicated to helping victims of natural disasters. We are committed to using 100% of donations to provide relief to those affected by disasters</div>
                     <br />
-                    <div className='donate-info-subhead'>How we works</div>
+                    <div className='donate-info-subhead'>How we work</div>
                     <br />
-                    <div className='donate-info'>When you donate to our website, your donation will be processed immediately.   We will then distribute the funds to our partners in the field, who will use them to provide food, water, shelter, and other essential supplies to those in need.</div>
+                    <div className='donate-info'>When you donate to our website, your donation will be processed immediately. We will then distribute the funds to our partners in the field, who will use them to provide food, water, shelter, and other essential supplies to those in need.</div>
                     <br />
                     <div className='donate-info-subhead'>Our partners</div>
                     <br />
-                    <div className='donate-info'>We partner with local organizations in the affected areas to ensure that your donations are used effectively.  Our partners have a proven track record of providing relief to victims of natural disasters.</div>
+                    <div className='donate-info'>We partner with local organizations in the affected areas to ensure that your donations are used effectively. Our partners have a proven track record of providing relief to victims of natural disasters.</div>
                     <br />
                     <div className='donate-info-subhead'>Thank you for your support</div>
                     <br />
-                    <div className='donate-info'>Your donation will help make a difference in the lives of those who have been affected by a natural disaster.  Thank you for your generosity.</div>
+                    <div className='donate-info'>Your donation will help make a difference in the lives of those who have been affected by a natural disaster. Thank you for your generosity.</div>
                 </div>
             </div>
 
-            <button className="donate-button" onClick={() => alert('Thank you for your support')}>
-                DONATE
+            <button
+                className="donate-button"
+                style={{ backgroundColor: buttonState.bgColor }}
+                onClick={handleButtonClick}
+            >
+                {buttonState.text}
             </button>
-
         </div>
-    )
+    );
 }
 
-export default Donate
+export default Donate;
