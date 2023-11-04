6248d36e6d99de43347d927d490c51f287e56c7b
Phoom <phoombnw@gmail.com>
2023-11-04 21:39:19 +0700

    home
    +images

---
+++ notify/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home-page-container'>
+      <div className="donate-body">
+        <div className="donate-body-left">
+          {/* Map here */}
+        </div>
+        <div className="donate-body-right">
+          <div className="detail-block">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
