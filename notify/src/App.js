def77e6bd4cb241df80744571d48429d879ddb21
kittipatdew1 <86704528+kittipatdew1@users.noreply.github.com>
2023-11-06 21:59:38 +0700

    delect about page

--- notify/src/App.js
+++ notify/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/navbar';
 import SignIn from './pages/sign-in-page/SignIn';
 import Register from './pages/register-page/Register';
-import About from './pages/About-us/about';
 import Emergency from './pages/Emergency/emergency'
 import EmergencyCard from './components/EmergencyCard';
 import Fotterbottom from './components/footerbottom'
