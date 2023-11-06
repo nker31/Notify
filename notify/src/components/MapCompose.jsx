7b64a24238edc4988edcde35ae19094a51c1f60e
Phoom <phoombnw@gmail.com>
2023-11-06 19:38:13 +0700

    home
    +resize marker
    +click alert

--- notify/src/components/MapCompose.jsx
+++ notify/src/components/MapCompose.jsx
@@ -4,11 +4,15 @@ import fire from '../images/fire.png'
 
 function MapCompose({ lat, lng, zoom, disableUI}) {
     const position = { lat, lng };
+    const icon = {
+        url: fire,
+        scaledSize: new window.google.maps.Size(30, 30),
+      };
     return (
         <APIProvider apiKey="AIzaSyAYWRedRUK6_0ol2PkQN3SJWgRfaO4dBY8">
             <Map center={position} zoom={zoom} disableDefaultUI={disableUI} mapId={'b75c0b5de2f2fec7'}>
                 {/* Add your map markers and other elements here */}
-                <Marker position={position} icon={fire} />
+                <Marker position={position} icon={icon}  onClick={() => alert("click!")}/>
             </Map>
         </APIProvider>
     )
