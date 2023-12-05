9ec3cf986be3130f50014bf79ce6a5cc34a060cf
Surabadee Intanai <bankoffirst@gmail.com>
2023-12-05 13:17:17 +0700

    auth
    notify folder
    + use a validator to validate input in Register.jsx.
    + use sweetalert2 to popup message error and success.
    + focus() on input error.
    
    notify-backend folder
    + hash password with bcrypt.
    + randomToken from crypto.
    + create token user with jasonwebtoken when user login.
    + for SignIn form check Invalid email and password.
    + for Register form check record existing user in database, email and username must be unique from the data in the database.

--- notify/src/pages/register-page/Register.jsx
+++ notify/src/pages/register-page/Register.jsx
@@ -1,24 +1,129 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './Register.css'
 import axios from 'axios'
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import validator from 'validator';
+import Swal from 'sweetalert2'
 
 function Register() {
-    const [email, setEmail] = useState()
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
-    const [confirmPassword, setComfirmpassword] = useState()
-    const [phoneNumber, setPhonenumber] = useState()
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setComfirmPassword] = useState('')
+    const [phoneNumber, setPhonenumber] = useState('')
+    const navigate = useNavigate()
+
+    const emailRef = useRef();
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+    const phoneNumberRef = useRef();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        axios.post('http://localhost:3001/register', {email, username, password, phoneNumber})
-        .then(result => console.log(result))
-        .catch(err => console.log(err))
+        if (!validator.isEmail(email)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops!',
+                text: 'Please enter a valid email address',
+                showConfirmButton: false,
+                timer: 1500,
+            }).then(() => {
+                setTimeout(() => {
+                    emailRef.current.focus();
+                }, 500);
+            });
+            return;
+        }
 
-    }
+        if (!validator.isLength(username, { min: 6, max: 20 })) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Username Incorrect',
+                text: 'Username must between 6 and 20 characters.',
+                showConfirmButton: false,
+                timer: 1500,
+            }).then(() => {
+                setTimeout(() => {
+                    usernameRef.current.focus();
+                }, 500);
+            });
+            return;
+        }
+
+        if (!validator.isLength(password, { min: 6 })) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Password Incorrect',
+                text: 'Password must be atleast 6 characters.',
+                showConfirmButton: false,
+                timer: 1500,
+            }).then(() => {
+                setTimeout(() => {
+                    passwordRef.current.focus();
+                }, 500);
+            });
+            return;
+        }
+
+        if (!validator.equals(password, confirmPassword)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Password Mismatch',
+                text: 'Please ensure your passwords match.',
+                showConfirmButton: false,
+                timer: 1500,
+            }).then(() => {
+                setTimeout(() => {
+                    confirmPasswordRef.current.focus();
+                }, 500);
+            });
+            return;
+        }
+
+        if (!validator.isMobilePhone(phoneNumber, 'th-TH')) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Invalid Phone Number',
+                text: 'Please ensure your phone number on th-TH',
+                showConfirmButton: false,
+                timer: 1500,
+            }).then(() => {
+                setTimeout(() => {
+                    phoneNumberRef.current.focus();
+                }, 500);
+            });
+            return;
+        }
+
+        axios.post('http://localhost:3001/api/auth/register', { email, username, password, phoneNumber })
+            .then(result => {
+                console.log(result)
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Register Successfully',
+                    showConfirmButton: false,
+                    timer: 1500,
+                }).then(() => {
+                    navigate('/login');
+                });
+            })
+            .catch(err => {
+                if (err.response){
+                    const errorMessage = err.response.data.error;
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Registration Failed',
+                        text: errorMessage,
+                        showConfirmButton: false,
+                        timer: 1500,
+                    });  
+                } else {
+                    console.log(err)
+                }
+            });
+    };
 
     return (
         <div className='register-bg'>
@@ -32,6 +137,7 @@ function Register() {
                                 placeholder="Email"
                                 name='email'
                                 onChange={(e) => setEmail(e.target.value)}
+                                ref={emailRef}
                             />
 
                             <input
@@ -39,6 +145,7 @@ function Register() {
                                 placeholder="Username"
                                 name='username'
                                 onChange={(e) => setUsername(e.target.value)}
+                                ref={usernameRef}
                             />
 
                             <input
@@ -46,13 +153,15 @@ function Register() {
                                 placeholder="Password"
                                 name='password'
                                 onChange={(e) => setPassword(e.target.value)}
+                                ref={passwordRef}
                             />
 
                             <input
                                 type="password"
                                 placeholder="Confirm Password"
                                 name='confirmPassword'
-                                onChange={(e) => setComfirmpassword(e.target.value)}
+                                onChange={(e) => setComfirmPassword(e.target.value)}
+                                ref={confirmPasswordRef}
                             />
 
                             <input
@@ -60,6 +169,7 @@ function Register() {
                                 placeholder="Phone number"
                                 name='phoneNumber'
                                 onChange={(e) => setPhonenumber(e.target.value)}
+                                ref={phoneNumberRef}
                             />
 
                             <button className="register-button" type="submit">Register</button>
@@ -68,12 +178,11 @@ function Register() {
                         <div className="register-box-line"></div>
                         <Link to='/login'><button className='register-no-account-button'>Already a member?</button></Link>
                     </div>
-              </div>
-                        </div>
-    </div>
-    </>
-  )
 
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Register
