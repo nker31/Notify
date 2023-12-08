67d499a4afcdaefb80162b02abb91fed3f8b6189
mofinkc51 <82575774+mofinkc51@users.noreply.github.com>
2023-12-08 15:59:59 +0700

    update

--- notify-backend/controller/marker_controller.js
+++ notify-backend/controller/marker_controller.js
@@ -43,7 +43,7 @@ export const getDataById = (req, res) => {
     .catch(err => res.json(err))
 }
 export const deleteData = (req, res) => {
-    markersModel.deleteMany({})
+    markersModel.deleteMany({name: req.params.id})
     .then(markers => res.json(markers))
     .catch(err => res.json(err))
 }
